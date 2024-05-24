bb619ecd1d297a1b3ff904d84ee59c55ec598a77
aifa <antonis@typesystem.xyz>
2024-05-24 12:43:44 +0200

    retry

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 # Use an official Python runtime as a parent image
 FROM python:3.10-slim
 
-RUN mkdir /app
 WORKDIR /app
-
-RUN mkdir -p /inputs 
-RUN mkdir -p /outputs
 
 # Install any needed packages specified in requirements.txt
 #RUN pip install --no-cache-dir -r requirements.txt
 
-ENV OUTPUT_DIR="/outputs/"
-
-ADD hello.py /app/hello.py
+COPY . /app
 # Run the script when the container launches
-ENTRYPOINT ["python3", "-u", "/app/hello.py"]  # Replace with your script name and IPFS link if static, or handle args in entrypoint
+CMD ["python", "hello.py"]  # Replace with your script name and IPFS link if static, or handle args in entrypoint
--- hello.py
+++ hello.py
@@ -1,19 +1,20 @@
-import os
 import sys
 
 
 
 
 def submit():
-    output_folder = os.getenv("OUTPUT_DIR")
-    if output_folder:
-        output_file = os.path.join(output_folder, "response.txt")
-        with open(output_file, "w") as file:
-            file.write("Hello, world!")
-            print(f"Response saved to {output_file}")
-    else:
-        print("OUTPUT environment variable not defined. Response will not be saved.", file=sys.stderr)
-    return 1
+    print("Hello, world!")
+    return "Hello world";
+    #output_folder = os.getenv("OUTPUT_DIR")
+    #if output_folder:
+    #    output_file = os.path.join(output_folder, "response.txt")
+    #    with open(output_file, "w") as file:
+    #        file.write("Hello, world!")
+    #        print(f"Response saved to {output_file}")
+    #else:
+    #    print("OUTPUT environment variable not defined. Response will not be saved.", file=sys.stderr)
+    #return 1
 
 
 
@@ -22,5 +23,5 @@ def submit():
 if __name__ == "__main__":
     #ipfs_url = "https://bafybeib76s7igm5ncpg3n3eno64bjhak62ua2gyqzdruaxvejngh4inxui.ipfs.w3s.link/Pope-Francis-Coat.jpg"
     # Parse command-line arguments
-    submit()
+    print(submit(), file=sys.stdout)
     
